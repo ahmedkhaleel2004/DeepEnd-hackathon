54ccdb2852917546c354d5a430108499818d8938
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-02-04 00:18:40 -0500

    move main into one page

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -4,12 +4,12 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/lib/hooks/use-auth";
 import SidebarContainer from "@/components/component/sidebar/sidebar-container";
+import { ModeToggle } from "@/components/component/mode-toggle";
+import Image from "next/image";
+import Link from "next/link";
+import Timeline from "@/components/component/timeline/timeline";
 
-interface MainLayoutProps {
-  children: React.ReactNode;
-}
-
-function MainLayout({ children }: MainLayoutProps) {
+function Page() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [sidebarOpen, setSidebarOpen] = useState(true);
   const [isHovered, setIsHovered] = useState(false);
@@ -36,11 +36,30 @@ function MainLayout({ children }: MainLayoutProps) {
           className="flex h-full w-full flex-col overflow-y-auto"
           ref={divRef}
         >
-          {children}
+          <>
+            <div className="m-8 flex items-center justify-between">
+              <div className="flex items-center">
+                <Image
+                  src="/deependlogo.svg"
+                  alt="DeepEnd Logo"
+                  width={40}
+                  height={40}
+                  className="dark:invert"
+                />
+                <Link className="ml-4 text-3xl font-semibold" href="/">
+                  DeepEnd
+                </Link>
+              </div>
+              <ModeToggle />
+            </div>
+            <main className="flex justify-center">
+              <Timeline uid="test" projectIndex={0} />
+            </main>
+          </>
         </div>
       </div>
     </div>
   );
 }
 
-export default MainLayout;
+export default Page;
