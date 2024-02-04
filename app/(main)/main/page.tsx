54b9331728292c39338f4a4c452252eb309a5e9b
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-02-03 23:06:53 -0500

    woops

--- app/(main)/main/page.tsx
+++ app/(main)/main/page.tsx
@@ -4,13 +4,7 @@ import React from "react";
 import { ModeToggle } from "@/components/component/mode-toggle";
 import Image from "next/image";
 import Link from "next/link";
-<<<<<<< HEAD
 import Timeline from "@/components/component/timeline/timeline";
-=======
-
-import { CardOfTimeline } from "@/components/component/Main/CardTimeline";
-import { ProgressBar } from "@/components/component/Main/ProgressBar";
->>>>>>> 02078ffc0febead79fb7be01040320f8797d0a54
 
 const Main = () => {
   return (
@@ -30,19 +24,8 @@ const Main = () => {
         </div>
         <ModeToggle />
       </div>
-<<<<<<< HEAD
       <main className="flex justify-center">
         <Timeline uid="test" projectIndex={0} />
-=======
-      <main className="flex items-center justify-center p-10">
-        <div className="flex w-3/4 flex-col items-center">
-          <CardOfTimeline
-            Title="Timeline"
-            Description="Select a date range to view the timeline"
-          />
-          <ProgressBar />
-        </div>
->>>>>>> 02078ffc0febead79fb7be01040320f8797d0a54
       </main>
     </>
   );
